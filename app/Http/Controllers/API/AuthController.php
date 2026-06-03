f0711268b2f76fdcf6aca085d9ef4bec9a19eb11
CYkir <mvzakiir@gmail.com>
2026-06-04 02:17:06 +0700

    Fix import statement for Lecturer model in AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lecturer;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
