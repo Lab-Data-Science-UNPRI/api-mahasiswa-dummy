7af4c03f8de3aa8389690ede9fece2779b92f666
CYkir <mvzakiir@gmail.com>
2026-06-05 03:57:41 +0700

    Refactor LecturerResource and StudentResource to return detailed resource arrays

--- app/Http/Resources/LecturerResource.php
+++ app/Http/Resources/LecturerResource.php
@@ -7,13 +7,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class LecturerResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'nidn' => $this->nidn,
+            'code' => $this->code,
+            'name' => trim(($this->front_title ? $this->front_title . ' ' : '') . $this->name . ($this->rear_title ? ', ' . $this->rear_title : '')),
+            'short_name' => $this->short_name,
+            'email' => $this->email,
+            'handphone' => $this->handphone,
+            'program' => new ProgramResource($this->whenLoaded('program')),
+            'status' => $this->status,
+        ];
     }
 }
--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -7,13 +7,55 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class StudentResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'nim' => $this->nim,
+            'code' => $this->code,
+            'name' => $this->name,
+            'email' => $this->email,
+
+            'personal' => [
+                'place_of_birth' => $this->place_of_birth,
+                'date_of_birth' => $this->date_of_birth,
+                'sex' => $this->sex,
+                'religion' => $this->religion,
+                'blood' => $this->blood,
+            ],
+
+            'contact' => [
+                'handphone' => $this->handphone,
+                'phone' => $this->phone,
+                'address' => $this->address,
+                'village' => $this->village,
+                'district' => $this->district,
+                'town' => $this->town,
+                'province' => $this->province,
+                'postal_code' => $this->postal_code,
+            ],
+
+            'academic' => [
+                'program' => new ProgramResource($this->whenLoaded('program')),
+                'lecturer' => new LecturerResource($this->whenLoaded('lecturer')),
+                'current_year' => new YearResource($this->whenLoaded('currentYear')),
+                'current_stage' => new StageResource($this->whenLoaded('currentStage')),
+                'current_semester' => new SemesterResource($this->whenLoaded('currentSemester')),
+                'status' => $this->status,
+            ],
+
+            'school' => [
+                'from_school' => $this->from_school,
+                'school_majors' => $this->school_majors,
+                'graduate_year' => $this->graduate_year,
+            ],
+
+            'parents' => [
+                'father_name' => $this->father_name,
+                'mother_name' => $this->mother_name,
+                'parent_address' => $this->parent_address,
+                'parent_handphone' => $this->parent_handphone,
+            ],
+        ];
     }
 }
